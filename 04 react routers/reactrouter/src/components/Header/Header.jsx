016ecee7cf6eb0440c2d62af2c9d82a283ad911d
Aman <ak193566@gmail.com>
2024-02-20 14:40:18 +0530

    testing react top loading bar

--- 04 react routers/reactrouter/src/components/Header/Header.jsx
+++ 04 react routers/reactrouter/src/components/Header/Header.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import {Link, NavLink} from 'react-router-dom'
+import LoadingBar from 'react-top-loading-bar'
 
 function Header(){
+    // const ref = useRef(null);
+    const [progress, setProgress] = useState(0);
     return(
         <>
         <header className="shadow sticky z-50 top-0">
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <Link to="/" className="flex items-center">
+                    <Link to="/" className="flex items-center" 
+                    // onClick={()=>ref.current.complete()}
+                    >
                         <img
                             src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
                             className="mr-3 h-12"
@@ -17,13 +22,16 @@ function Header(){
                     <div className="flex items-center lg:order-2">
                         <Link
                             to="#"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none" 
+                            // onClick={()=>ref.current.complete()}
                         >
                             Log in
                         </Link>
                         <Link
                             to="#"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none" 
+                            
+                            // onClick={()=>ref.current.complete()}
                         >
                             Get started
                         </Link>
@@ -38,7 +46,8 @@ function Header(){
                                     to='/'
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-grey-700"}`
-                                    }
+                                    } 
+                                    // onClick={()=>ref.current.complete()}
                                 >
                                     Home
                                 </NavLink>
@@ -48,7 +57,8 @@ function Header(){
                                     to='/about'
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-grey-700"}`
-                                    }
+                                    } 
+                                    // onClick={()=>ref.current.complete()}
                                 >
                                     About
                                 </NavLink>
@@ -58,7 +68,8 @@ function Header(){
                                     to='/contact'
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-grey-700"}`
-                                    }
+                                    } 
+                                    // onClick={()=>ref.current.complete()}
                                 >
                                     Contact
                                 </NavLink>
@@ -68,7 +79,7 @@ function Header(){
                                     to='/github'
                                     className={({isActive}) =>
                                         `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0 ${isActive ? "text-orange-700" : "text-grey-700"}`
-                                    }
+                                    } onClick={()=>{setProgress(100)} }
                                 >
                                     GitHub
                                 </NavLink>
@@ -78,6 +89,12 @@ function Header(){
                 </div>
             </nav>
         </header>
+        {/* <LoadingBar color='#f11946' ref={ref} /> */}
+        <LoadingBar
+        color='#f11946'
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
         </>
     )
 }
